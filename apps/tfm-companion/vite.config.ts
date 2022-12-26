b4f8f0e7cbf1111be7e4c22726627e4b925df4db
Alex Malkevich <malkevich.alex@gmail.com>
2022-12-26 19:26:56 +0100

    Initial commit

---
+++ apps/tfm-companion/vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+
+import viteTsConfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  server: {
+    port: 4200,
+    host: 'localhost',
+  },
+
+  plugins: [
+    viteTsConfigPaths({
+      root: '../../',
+    }),
+  ],
+
+  test: {
+    globals: true,
+    cache: {
+      dir: '../../node_modules/.vitest',
+    },
+    environment: 'jsdom',
+    include: ['src/**/*.spec.{js,ts}'],
+  },
+});
