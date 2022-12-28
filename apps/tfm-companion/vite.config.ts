bc1fe8151f26cc148a9c87a374c9d806fde51d8a
Alex Malkevich <malkevich.alex@gmail.com>
2022-12-28 20:43:33 +0100

    feat(pwa): setup pwa with full offline cache

--- apps/tfm-companion/vite.config.ts
+++ apps/tfm-companion/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
-
 import viteTsConfigPaths from 'vite-tsconfig-paths';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   server: {
@@ -11,6 +11,37 @@ export default defineConfig({
   plugins: [
     viteTsConfigPaths({
       root: '../../',
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      injectRegister: 'inline',
+      manifest: {
+        name: 'Terraforming Mars Companion App',
+        short_name: 'TFM Companion',
+        description: 'Terraforming Mars Companion App',
+        theme_color: '#000000',
+        background_color: '#000000',
+        display: 'standalone',
+        orientation: 'portrait-primary',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+        navigateFallback: '/index.html',
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
     }),
   ],
 
